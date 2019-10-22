0a4461dc2c287d1184f94256c3334a1d47676494
姜赛 @jack.jiang <jiangsai0218@163.com>
2019-10-22 21:05:56 +0800

    导航栏下添加按钮

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Welcome to Flutter',
       debugShowCheckedModeBanner: false,//去掉右上角的debug标签
-      home: new Scaffold(
+      home: DefaultTabController(
+          length: 3,
+           child: Scaffold(
+          
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
           elevation: 5,//设置阴影
@@ -17,6 +20,13 @@ class MyApp extends StatelessWidget {
             icon: Icon(Icons.memory),
             tooltip: "Navigation",
             onPressed: () => debugPrint('点击了导航栏上的按钮'),
+          ),
+          bottom: TabBar(
+           tabs: <Widget>[
+             Tab(icon: Icon(Icons.local_activity),),
+             Tab(icon: Icon(Icons.local_airport),),
+             Tab(icon: Icon(Icons.local_atm),),
+           ], 
           ),
           actions: <Widget>[
             IconButton(
@@ -26,11 +36,28 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        body: HomeListView(),
+        // body: HomeListView(),
+        body: CustomTabBarView(),
+     
+           ),
       ),
       theme: ThemeData(
         primarySwatch: Colors.yellow
       ),
+    );
+  }
+}
+
+class CustomTabBarView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      children: <Widget>[
+       Icon(Icons.local_activity,size: 120,color: Colors.black12,),
+
+       Icon(Icons.local_airport,size: 120,color: Colors.blueGrey,),
+       Icon(Icons.local_bar,size: 120,color: Colors.blue,),
+      ],
     );
   }
 }
